00bcfc34c0f49c06da4e4d549badc7b6075ce2d7
Kevin <koolkev246@gmail.com>
2021-12-26 21:23:19 -0500

    Create Final-Value-of-Variable-After-Performing-Operations.java

---
+++ Java/Easy/Final-Value-of-Variable-After-Performing-Operations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X=0;
+        for (int i=0;i<operations.length;i++)
+        {
+            X+=theOps(operations[i]);
+        }
+        return X;
+    }
+    public int theOps(String operation)
+    {
+        if (operation.equals("++X")||operation.equals("X++"))
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
