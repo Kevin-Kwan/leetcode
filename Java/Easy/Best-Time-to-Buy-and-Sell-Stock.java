50d5fd7e2fe6787f5c640ff6e0616e9e704c3613
Kevin <koolkev246@gmail.com>
2022-11-12 02:27:36 -0500

    Create Best-Time-to-Buy-and-Sell-Stock.java

---
+++ Java/Easy/Best-Time-to-Buy-and-Sell-Stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i<prices.length;i++) {
+            if (prices[i]<min) {
+                min = prices[i];
+            }
+            if (prices[i]-min > maxProfit) {
+                maxProfit = prices[i]-min;
+            }
+        }
+        return maxProfit;
+    }
+}
