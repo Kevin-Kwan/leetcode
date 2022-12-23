5470d409e980f2e5e9f542a94818a4f845752fee
Kevin <koolkev246@gmail.com>
2022-12-23 18:00:06 -0500

    Create Valid-Anagram.java

---
+++ Java/Easy/Valid-Anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        if (sArray.length != tArray.length) {
+            return false;
+        }
+        for (int i = 0; i < sArray.length; i++) {
+            if (sArray[i]!=tArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
