54b9ae6b1a2fdc9747a2dc94164abbf677cb8137
Kevin <koolkev246@gmail.com>
2022-12-30 13:41:27 -0500

    Create Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree.java

---
+++ Java/Easy/Find-a-Corresponding-Node-of-a-Binary-Tree-in-a-Clone-of-That-Tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (original != null) {
+            if (original == target) {
+                return cloned;
+            } else {
+                TreeNode sameNode = getTargetCopy(original.left, cloned.left, target);
+                if(sameNode != null) {
+                    return sameNode;
+                }
+                sameNode = getTargetCopy(original.right, cloned.right, target);
+                if(sameNode != null) {
+                    return sameNode;
+                } 
+            }
+        }
+        return null;
+    }
+}
