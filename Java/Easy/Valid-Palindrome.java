2c8359e87f88ec47b36926d8daaea555f73bc710
Kevin <koolkev246@gmail.com>
2023-01-01 14:48:27 -0500

    Update Valid-Palindrome.java

--- Java/Easy/Valid-Palindrome.java
+++ Java/Easy/Valid-Palindrome.java
@@ -1,11 +1,14 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^A-Za-z0-9]","");
-        s = s.toLowerCase();
-        String reverse = "";
-        for(int i = s.length()-1; i>=0;i--) {
-            reverse += s.charAt(i);
+        String noPunctSpace = s.replaceAll("\\s|[^a-zA-Z0-9]","");
+        String newS = noPunctSpace.toLowerCase();
+        boolean answer = true;
+        for (int i = 0; i<(newS.length()-(newS.length()/2));i++) {
+            if (newS.charAt(i)!=newS.charAt(newS.length()-1-i)) {
+                answer = false;
+                break;
+            }
         }
-        return s.equals(reverse);
+        return answer;
     }
 }
