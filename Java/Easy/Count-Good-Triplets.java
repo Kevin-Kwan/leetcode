804c4a5715d6956019b94bfeb474b51cd5c11ce6
Kevin <koolkev246@gmail.com>
2023-01-03 21:40:15 -0500

    Create Count-Good-Triplets.java

---
+++ Java/Easy/Count-Good-Triplets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int i = 0;
+        int j = i + 1;
+        int k = j + 1;
+        int count = 0;
+        while (k<arr.length) {
+            if (Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c ) {
+                count++;
+            }
+            if (k<arr.length-1) {
+                k++;
+            } else if (k==arr.length-1 && j<arr.length-2) {
+                j++;
+                k=j+1;
+            } else if (j==arr.length-2) {
+                i++;
+                j=i+1;
+                k=j+1;
+            }
+        }
+        return count;
+    }
+}
