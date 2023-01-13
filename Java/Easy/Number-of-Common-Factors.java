a71b2183c7217882991afce06aa1dcf2ccfd632f
Kevin <koolkev246@gmail.com>
2023-01-13 00:01:55 -0500

    Create Number-of-Common-Factors.java

---
+++ Java/Easy/Number-of-Common-Factors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int commonFactors(int a, int b) {
+        int min = a < b ? a : b;
+        int answer = 0;
+        for(int i = 1; i <= min; i++) {
+            if(a%i == 0 && b%i == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
