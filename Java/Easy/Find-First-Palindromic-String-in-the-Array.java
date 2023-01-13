db40a7756e6c8721703d0978b19769acbb7d747c
Kevin <koolkev246@gmail.com>
2023-01-12 23:40:26 -0500

    Create Find-First-Palindromic-String-in-the-Array.java

---
+++ Java/Easy/Find-First-Palindromic-String-in-the-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String word:words) {
+            if (checkPalidrome(word)) {
+                return word;
+            }
+        }
+        return "";
+    }
+    public boolean checkPalidrome(String word) {
+        for (int i = 0; i <= word.length()/2; i++) {
+            if (word.charAt(i)!=word.charAt(word.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
