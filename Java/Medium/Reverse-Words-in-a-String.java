9b5ab3f72d2aa38120199f570b548dce1f668975
Kevin <koolkev246@gmail.com>
2023-01-23 23:49:00 -0500

    Create Reverse-Words-in-a-String.java

---
+++ Java/Medium/Reverse-Words-in-a-String.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] tempS = s.trim().split("\\s+");
+        String answer = "";
+        for(int i = tempS.length-1; i>0; i--) {
+            answer+=tempS[i]+" ";
+        }
+        return answer+tempS[0];
+    }
+}
