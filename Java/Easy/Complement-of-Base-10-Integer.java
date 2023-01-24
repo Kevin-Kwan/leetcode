88f83cf6486e1ec6c2f30aee66604583f83efff1
Kevin <koolkev246@gmail.com>
2023-01-24 00:01:12 -0500

    Create Complement-of-Base-10-Integer.java

---
+++ Java/Easy/Complement-of-Base-10-Integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        String binaryN = Integer.toBinaryString(n);
+        String newBinary = "";
+        for (int i = 0; i<binaryN.length();i++) {
+            if (binaryN.charAt(i)=='0') {
+                newBinary+="1";
+            } else {
+                newBinary+="0";
+            }
+        }
+        //System.out.println(newBinary);
+        return Integer.parseInt(newBinary,2);
+    }
+}
