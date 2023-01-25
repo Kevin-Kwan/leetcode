c835864173ca26abe1426e6010a307464de3b37e
Kevin <koolkev246@gmail.com>
2023-01-24 21:37:16 -0500

    Create Remove-Duplicates-from-Sorted-Array.java

---
+++ Java/Easy/Remove-Duplicates-from-Sorted-Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        TreeSet<Integer> hashset = new TreeSet<>();
+        for (int i = 0; i<nums.length; i++) {
+            hashset.add(nums[i]);
+        }
+        int j = 0;
+        for (int element:hashset) {
+            nums[j++] = element;
+        }
+        return hashset.size();
+    }
+}
