9f374f110874c0f4bae6e30ce3749bac605fe057
Kevin <koolkev246@gmail.com>
2023-01-28 02:44:07 -0500

    Create Maximum-Sum-of-an-Hourglass.java

---
+++ Java/Medium/Maximum-Sum-of-an-Hourglass.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int max = -1;
+        for (int i = 1; i<grid.length-1; i++) {
+            for (int j = 1; j<grid[0].length-1;j++) {
+                int hourglassValue = grid[i][j]+grid[i-1][j-1]+grid[i-1][j]+grid[i-1][j+1]+grid[i+1][j-1]+grid[i+1][j]+grid[i+1][j+1];
+                if (hourglassValue > max) {
+                    max = hourglassValue;
+                }
+            }
+        }
+        return max;
+    }
+}
