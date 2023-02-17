ff9a77867eee06f2479f8e8fa92721d995a57209
Kevin <koolkev246@gmail.com>
2023-02-16 21:53:58 -0500

    Create Find-the-Array-Concatenation-Value.java

---
+++ Java/Easy/Find-the-Array-Concatenation-Value.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        long answer = 0;
+        for (int i = 0, j = nums.length-1; i<j; i++,j--) {
+            answer+= Integer.parseInt(Integer.toString(nums[i]) + Integer.toString(nums[j]));
+        }
+        if (nums.length%2!=0) {
+            answer+=nums[nums.length/2];
+        }
+        return answer; 
+    }
+}
