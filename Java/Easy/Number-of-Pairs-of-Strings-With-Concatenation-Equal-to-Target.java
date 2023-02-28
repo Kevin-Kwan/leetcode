4f143c83b8e785065a2946882f5ce9598255dbf2
Kevin <koolkev246@gmail.com>
2023-02-27 21:56:09 -0500

    Create Number-of-Pairs-of-Strings-With-Concatenation-Equal-to-Target.java

---
+++ Java/Easy/Number-of-Pairs-of-Strings-With-Concatenation-Equal-to-Target.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int answer = 0;
+        for (int i = 0; i<nums.length; i++) {
+            for (int j = 0; j<nums.length; j++) {
+                if (i!=j) {
+                    if (target.equals(nums[i]+nums[j])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
