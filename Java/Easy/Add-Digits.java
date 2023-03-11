a93880eec22ff870be61b5e61de20e548ef26d93
Kevin <koolkev246@gmail.com>
2023-03-11 13:55:08 -0500

    Create Add-Digits.java

---
+++ Java/Easy/Add-Digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int num) {
+        int answer = num;
+        String temp = Integer.toString(num);
+        while (temp.length()!=1) {
+            answer = 0;
+            for (int i = 0; i < temp.length(); i++) {
+                answer+= temp.charAt(i)-'0';
+            }
+            temp = Integer.toString(answer);
+        }
+        return answer;
+    }
+}
