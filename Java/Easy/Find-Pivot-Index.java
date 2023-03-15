d3929f12cc2bbd8b4280c4b283bab5deb6713cfd
Kevin <koolkev246@gmail.com>
2023-03-15 12:00:33 -0400

    Create

---
+++ Java/Easy/Find-Pivot-Index.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int index = 0;
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 1; i<nums.length; i++) {
+            rightSum+=nums[i];
+        }
+        if (leftSum == rightSum) {
+            return index;
+        } else {
+            for (int i = 1; i<nums.length; i++) {
+            leftSum += nums[i-1];
+            rightSum -= nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+        }
+        }
+        return -1;
+    }
+}
