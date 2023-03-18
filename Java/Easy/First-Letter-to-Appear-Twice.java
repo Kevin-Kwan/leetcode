9f37ecebf85cbaccae6c1261c1de0982e2201d63
Kevin <koolkev246@gmail.com>
2023-03-17 20:57:41 -0400

    Create First-Letter-to-Appear-Twice.java

---
+++ Java/Easy/First-Letter-to-Appear-Twice.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int[] smallDict = new int[26];
+        for (int i = 0; i<s.length(); i++) {
+            if (++smallDict[s.charAt(i)-'a']==2) {
+                return s.charAt(i);
+            }
+        }
+        return 0;
+    }
+}
