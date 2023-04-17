4017a617858c0342942704dbc2549bc0dacaf552
Kevin <koolkev246@gmail.com>
2023-04-17 00:09:06 -0400

    Create With-the-Greatest-Number-of-Candies.java

---
+++ Java/Easy/With-the-Greatest-Number-of-Candies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> answer = new LinkedList<>();
+        int max = candies[0];
+        for (int candy : candies) {
+            if (candy > max) {
+                max = candy;
+            }
+        }
+        for (int i = 0; i<candies.length; i++) {
+            answer.add(candies[i]+extraCandies>=max);
+        }
+        return answer;
+
+    }
+}
