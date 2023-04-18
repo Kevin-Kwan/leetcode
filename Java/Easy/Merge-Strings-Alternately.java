c798301fd06adc25cb396b585c4365ab059ee4d7
Kevin <koolkev246@gmail.com>
2023-04-18 00:10:06 -0400

    Create Merge-Strings-Alternately.java

---
+++ Java/Easy/Merge-Strings-Alternately.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int i = 0;
+        StringBuilder answer = new StringBuilder();
+        int w1length = word1.length();
+        int w2length = word2.length();
+        while (i<w1length && i<w2length) {
+            answer.append(word1.charAt(i)).append(word2.charAt(i++));
+        }
+        answer.append(word1.substring(i,w1length)).append(word2.substring(i,w2length));
+        return answer.toString();
+    }
+}
