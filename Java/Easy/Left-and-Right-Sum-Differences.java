ffc3720332bf5fa55a4df6428f1c49c3b12a8343
Kevin <koolkev246@gmail.com>
2023-04-19 00:04:57 -0400

    Create Left-and-Right-Sum-Differences.java

---
+++ Java/Easy/Left-and-Right-Sum-Differences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] leftRigthDifference(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        if (nums.length == 1) {
+            return new int[1];
+        }
+        for (int i = 0; i<nums.length-1; i++) {
+            right[right.length-2-i] += nums[nums.length-1-i] + right[right.length-1-i];
+            left[i+1] = nums[i]+left[i];
+        }
+        for (int j = 0; j<nums.length; j++) {
+            nums[j] = Math.abs(left[j]-right[j]);
+        }
+        return nums;
+    }
+}
