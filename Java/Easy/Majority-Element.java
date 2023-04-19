c7375d5a7e087925d29a300a0451f75b3db26e0c
Kevin <koolkev246@gmail.com>
2023-04-19 16:11:40 -0400

    Create Majority-Element.java

---
+++ Java/Easy/Majority-Element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int threshold = nums.length/2;
+        Map<Integer, Integer> storage = new HashMap<>();
+        for (int i = 0; i<nums.length; i++) {
+            int temp = storage.getOrDefault(nums[i],0)+1;
+            if (temp>threshold) {
+                return nums[i];
+            }
+            storage.put(nums[i],temp);
+        }
+        return 0;
+    }
+}
