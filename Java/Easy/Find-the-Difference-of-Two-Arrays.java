d49651c6088d9c72c654afd2e957d2fc4659f487
Kevin <koolkev246@gmail.com>
2023-05-03 04:10:58 -0400

    Create Find-the-Difference-of-Two-Arrays.java

---
+++ Java/Easy/Find-the-Difference-of-Two-Arrays.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> answer = new LinkedList<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int num1:nums1) {
+            set1.add(num1);
+        }
+        for (int num2:nums2) {
+            set2.add(num2);
+        }
+        answer.add(new LinkedList<>(set1));
+        answer.get(0).removeAll(set2);
+        answer.add(new LinkedList<>(set2));
+        answer.get(1).removeAll(set1);
+        return answer;
+    }
+}
