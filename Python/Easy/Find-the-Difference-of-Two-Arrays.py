a69d09b017843159c79bcacd6391b7ed30f79bd1
Kevin <koolkev246@gmail.com>
2023-05-03 02:22:48 -0400

    Create Find-the-Difference-of-Two-Arrays.py

---
+++ Python/Easy/Find-the-Difference-of-Two-Arrays.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findDifference(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[List[int]]
+        """
+
+        return [list(set(nums1)-set(nums2)),list(set(nums2)-set(nums1))]
