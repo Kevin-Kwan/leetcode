64c7bff990b312b471199fc939a5a7fcf8bb7acb
Kevin <koolkev246@gmail.com>
2023-05-08 18:35:43 -0400

    Create Minimum-Sum-of-Four-Digit-Number-After-Splitting-Digits.java

---
+++ Java/Easy/Minimum-Sum-of-Four-Digit-Number-After-Splitting-Digits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimumSum(int num) {
+        int[] nums = new int[String.valueOf(num).length()];
+        for (int i = nums.length-1; i>=0;i--) {
+            nums[i] = num%10;
+            num/=10;
+        }
+        Arrays.sort(nums);
+        return (nums[0]*10+nums[1]*10+nums[2]+nums[3]);
+    }
+}
