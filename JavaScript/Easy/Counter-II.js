52886163f21a35839f1794186f252c5630590a4b
Kevin <koolkev246@gmail.com>
2023-05-08 18:23:17 -0400

    Create Counter-II.js

---
+++ JavaScript/Easy/Counter-II.js
@@ -0,0 +1,19 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var ans = init || 0;
+    return {
+        increment: () => ++ans,
+        decrement: () => --ans,
+        reset: () => ans = init,
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
