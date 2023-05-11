f709ed52d13a5566560261f3d04a00cc326658ff
Kevin <koolkev246@gmail.com>
2023-05-10 21:21:59 -0400

    Create Function-Composition.js

---
+++ JavaScript/Easy/Function-Composition.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+	return function(x) {
+        if (functions.length == 0) {
+            return x;
+        }
+        let tempX = x;
+        for (const func of functions.reverse()) {
+            tempX = func(tempX);
+        }
+        return tempX;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
