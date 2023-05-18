711e9f4d504ed651a5e4cc7349444faec79e8c41
Kevin <koolkev246@gmail.com>
2023-05-17 22:02:50 -0400

    Create minimum-number-of-vertices-to-reach-all-nodes.java

---
+++ Java/Medium/minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        // seems like we need to find all the source nodes
+        int[] edgesIn = new int[n];
+        List<Integer> answer = new LinkedList<>();
+        for (List<Integer> pair:edges) {
+            edgesIn[pair.get(1)]++;
+        }
+        for (int i = 0; i<n; i++) {
+            if (edgesIn[i]==0) {
+                answer.add(i);
+            }
+        }
+        return answer;
+    }
+}
