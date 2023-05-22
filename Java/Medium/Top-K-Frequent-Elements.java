4b0b8b2fd5c9f8f9609e9906e0aeed67f61f59ef
Kevin <koolkev246@gmail.com>
2023-05-22 11:15:57 -0400

    Create Top-K-Frequent-Elements.java

---
+++ Java/Medium/Top-K-Frequent-Elements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] answer = new int[k];
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            frequencyMap.put(num,frequencyMap.getOrDefault(num,0)+1);
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> frequencyMap.get(a) - frequencyMap.get(b));
+        for (int num : frequencyMap.keySet()) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            answer[i] = minHeap.poll();
+        }
+        return answer;
+    }
+}
