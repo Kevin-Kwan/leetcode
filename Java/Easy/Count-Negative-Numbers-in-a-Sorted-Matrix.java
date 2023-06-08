ae817609dee475c73307b0e5e6cba2a61d308a15
Kevin <koolkev246@gmail.com>
2023-06-07 21:41:01 -0400

    Update Count-Negative-Numbers-in-a-Sorted-Matrix.java

--- Java/Easy/Count-Negative-Numbers-in-a-Sorted-Matrix.java
+++ Java/Easy/Count-Negative-Numbers-in-a-Sorted-Matrix.java
@@ -1,13 +1,29 @@
 class Solution {
     public int countNegatives(int[][] grid) {
         int answer = 0;
-        for (int m = 0; m<grid.length; m++) {
-            for (int n = 0; n<grid[0].length; n++) {
-                if (grid[m][n]<0) {
-                    answer++;
-                }
+        int mSize = grid.length;
+        int nSize = grid[0].length;
+        for (int m = 0; m<mSize; m++) {
+            int index = findFirstNegative(grid[m]);
+            if (index>=0) {
+                answer+=nSize-index;
             }
         }
         return answer;
+    }
+    public static int findFirstNegative(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < 0) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
     }
 }
