4767db496609a8bf266334dab18f3424ef6176d7
Kevin <koolkev246@gmail.com>
2023-06-27 16:29:40 -0400

    Update Same-Tree.java

--- Java/Easy/Same-Tree.java
+++ Java/Easy/Same-Tree.java
@@ -15,13 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if ((p==null && q!=null) || (p!=null && q==null)) {
-            return false;
-        }
         if (p==null && q==null) {
             return true;
         }
-        if (p.val!=q.val) {
+        if ((p==null && q!=null) || (p!=null && q==null) || (p.val!=q.val)) {
             return false;
         }
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
