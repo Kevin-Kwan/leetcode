c18f1d12897d7d39e425872852cf967dfcef23d6
Kevin Kwan <koolkev246@gmail.com>
2023-09-24 20:32:21 -0400

    Create Find-the-Difference.py

---
+++ Python/Easy/Find-the-Difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s = sorted(s)
+        t = sorted(t)
+        for i in range(len(s)):
+            if s[i]!=t[i]:
+                return t[i]
+        return t[len(t)-1]
