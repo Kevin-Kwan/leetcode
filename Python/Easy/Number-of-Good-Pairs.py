d0e4fd5125c5615338b767affc590a26edf19a53
Kevin Kwan <koolkev246@gmail.com>
2023-10-02 22:35:53 -0400

    Update Number-of-Good-Pairs.py

--- Python/Easy/Number-of-Good-Pairs.py
+++ Python/Easy/Number-of-Good-Pairs.py
@@ -1,3 +1,3 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        return sum(1 for i in range(len(nums)) for j in range(len(nums)) if nums[i] == nums[j] and i < j)
+        return sum(1 for i in range(len(nums)) for j in range(i+1,len(nums)) if nums[i] == nums[j] and i < j)
