5b22461d9638cb8dd4708e6130e383bc965e1182
Kevin Kwan <koolkev246@gmail.com>
2023-11-18 21:44:09 -0500

    Optimized Reduction-Operations-to-Make-the-Array-Elements-Equal.py

--- Python/Medium/Reduction-Operations-to-Make-the-Array-Elements-Equal.py
+++ Python/Medium/Reduction-Operations-to-Make-the-Array-Elements-Equal.py
@@ -1,15 +1,9 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        # list.sort(nums)
         counter = Counter(nums)
         answer = 0
         single_list = list(counter.keys())
         list.sort(single_list)
-        next_largest = single_list[0]
-        running_ops = 1
-        # print(single_list)
         for i in range(1,len(single_list)):
-            if single_list[i]>next_largest:
-                answer+=counter.get(single_list[i])*running_ops
-                running_ops+=1
+            answer+=counter.get(single_list[i])*i
         return answer
