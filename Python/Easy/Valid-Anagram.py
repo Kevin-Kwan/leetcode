fb7102410d981a30943e5da2b66f25af1b1bad2e
Kevin Kwan <koolkev246@gmail.com>
2023-12-15 21:53:42 -0500

    Create Valid-Anagram.py

---
+++ Python/Easy/Valid-Anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for i in range(len(s)):
+            count[s[i]] = count.get(s[i], 0) + 1
+            count[t[i]] = count.get(t[i], 0) - 1
+        for val in count.values():
+            if val != 0:
+                return False
+        return True
