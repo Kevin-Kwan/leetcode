fe62cc799bbad3cc80bb37e644f05572e4f7de6e
Kevin Kwan <koolkev246@gmail.com>
2023-12-29 22:48:48 -0500

    Create Minimum-Number-Game.py

---
+++ Python/Easy/Minimum-Number-Game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        n = len(nums)
+        for i in range(0,n,2):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
