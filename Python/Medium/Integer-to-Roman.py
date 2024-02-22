e4547b7a693b0d8339025a348405cc7ed5ba14af
Kevin Kwan <koolkev246@gmail.com>
2024-02-22 17:59:05 -0500

    Create Integer-to-Roman.py

---
+++ Python/Medium/Integer-to-Roman.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        answer = ""
+        numToRomanMap = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+        for rep in numToRomanMap:
+            while num>=rep[0]:
+                num-=rep[0]
+                answer+=rep[1]
+        return answer
