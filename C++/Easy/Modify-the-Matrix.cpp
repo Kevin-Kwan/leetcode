7b447b981ef2abf2a5ce59b5841085e8c0ca33cb
Kevin Kwan <koolkev246@gmail.com>
2024-03-09 22:47:56 -0500

    Update Modify-the-Matrix.cpp

--- C++/Easy/Modify-the-Matrix.cpp
+++ C++/Easy/Modify-the-Matrix.cpp
@@ -3,21 +3,14 @@ public:
     vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int maxes[n];
         for (int col = 0; col<n; col++) {
-            int max = 0;
+            int colMax = 0;
             for (int row = 0; row<m; row++) {
-                if (matrix[row][col]>max) {
-                    max = matrix[row][col];
-                }
+                colMax = max(colMax, matrix[row][col]);
             }
-            maxes[col] = max;
-            cout << max;
-        }
-        for (int col = 0; col<n; col++) {
             for (int row = 0; row<m; row++) {
                 if (matrix[row][col]==-1) {
-                    matrix[row][col]=maxes[col];
+                    matrix[row][col]=colMax;
                 }
             }
         }
