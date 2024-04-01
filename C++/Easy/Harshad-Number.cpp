52f2cbb669d7ed2d338b4c53ad6ac375e7a84664
Kevin Kwan <koolkev246@gmail.com>
2024-03-31 23:57:57 -0400

    Create Harshad-Number.cpp

---
+++ C++/Easy/Harshad-Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int num = x;
+        while (num > 0) {
+            sum+=num%10;
+            num/=10;
+        }
+        if (x%sum==0) {
+            return sum;
+        } else {
+            return -1;
+        }
+    }
+};
