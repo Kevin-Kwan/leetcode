063b9f8a8b048ab84a67e6a23cb60126677ed0b8
Kevin <koolkev246@gmail.com>
2025-12-20 15:39:52 -0500

    Refactor minDeletionSize for clarity and efficiency

--- Java/Easy/Delete-Columns-to-Make-Sorted.java
+++ Java/Easy/Delete-Columns-to-Make-Sorted.java
@@ -1,14 +1,16 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
+        int n = strs.length; // rows
+        int m = strs[0].length(); // cols
         int answer = 0;
-        for (int i = 0; i<strs[0].length(); i++) {
-            for (int j = 0; j < strs.length-1; j++) {
+        for (int i = 0; i<m; i++) {
+            for (int j = 0; j<n-1; j++) {
                 if (strs[j].charAt(i)>strs[j+1].charAt(i)) {
                     answer++;
                     break;
                 }
             }
-        }
+        } 
         return answer;
     }
 }
